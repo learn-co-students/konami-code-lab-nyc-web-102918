159bb0528af6a7e598cc47a4faf737f194ad236f
Matt Readout <matt.readout@gmail.com>
2018-12-11 12:58:33 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,21 @@ const codes = [
 
 function init() {
   // your code here
+  let index = 0;
+  document.body.addEventListener('keydown', function(event) {
+    console.log(event.key);
+    let key = event.key;
+    if (key === codes[index]) {
+      index++;
+      if (index === codes.length) {
+        alert('ding ding');
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  })
+
+
 }
