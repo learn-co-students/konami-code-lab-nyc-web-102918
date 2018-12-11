d89355e4568bce0f23e60fc1d8e9975bd7cee71d
ruthmesfun <ruth.mesfun@gmail.com>
2018-12-10 22:18:03 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,45 @@ const codes = [
   "b",
   "a"
 ];
+// let konami = (event) =>{
+//   let i = 0;
+//   const key = event.key
+
+//   if (key === codes[i]){
+//     ++i;
+//     if (i === codes.length){
+//       alert("Woot Woot!");
+//       i = 0;
+//     }
+//   }
+//   else{
+//     i = 0;
+//   }
+// }
+
 
 function init() {
-  // your code here
+  let i = 0;
+  document.body.addEventListener('keydown', (event) => {
+    console.log("i", i)
+    console.log("event:",event)
+    const key = event.key
+    // if else statement
+    console.log("key:",key)
+    console.log('supposed to press', codes[i])
+    console.log('length of code',codes.length)
+    if (key === codes[i]){
+      i++;
+      console.log('i2:', i) 
+      if (i === codes.length){
+        console.log(event.key)
+        alert("Woot Woot!");
+        i = 0;
+      }//if statement
+    } //close if /else
+    else{
+      // alert("YOU LOST");
+      i = 0;
+    }// else
+  });
 }
